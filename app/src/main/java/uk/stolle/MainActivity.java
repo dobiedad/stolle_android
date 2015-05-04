f759e9f12d14d2e09641c2635f6348c22f610ffb
dobiedad <leomdiv@gmail.com>
2015-05-04 16:06:28 +0100

    loading indicator/ load onResume

--- app/src/main/java/uk/stolle/MainActivity.java
+++ app/src/main/java/uk/stolle/MainActivity.java
@@ -1,46 +1,88 @@
 package uk.stolle;
+        import android.os.Bundle;
+        import android.app.Activity;
+        import android.graphics.Bitmap;
+        import android.view.KeyEvent;
+        import android.view.Menu;
+        import android.view.View;
+        import android.webkit.WebView;
+        import android.webkit.WebViewClient;
+        import android.widget.ProgressBar;
+        import android.support.v7.app.ActionBarActivity;
+        import android.os.Bundle;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
 
+/*
+ * Demo of creating an application to open any URL inside the application and clicking on any link from that URl
+should not open Native browser but  that URL should open in the same screen.
 
-public class MainActivity extends ActionBarActivity {
+- Load WebView with progress bar
+ */
+public class MainActivity extends Activity {
+
+    WebView web;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
-        WebView myWebView = (WebView) findViewById(R.id.webview);
-        myWebView.loadUrl("http://www.stolle.xyz/?app");
-        WebSettings webSettings = myWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
+
+
+        web = (WebView) findViewById(R.id.webview1);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        web.setBackgroundColor(0x372726);
+        web.setWebViewClient(new myWebClient());
+        web.getSettings().setJavaScriptEnabled(true);
+        web.loadUrl("http://www.stolle.xyz.?app");
+
+
     }
 
+    public class myWebClient extends WebViewClient
+    {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            // TODO Auto-generated method stub
+            super.onPageFinished(view, url);
+            progressBar.setVisibility(View.GONE);
+        }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            // TODO Auto-generated method stub
+            super.onPageStarted(view, url, favicon);
+        }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // TODO Auto-generated method stub
+            view.loadUrl(url);
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
+
     }
+    @Override
+    public void onResume() {
+        super.onResume();
+        web.reload();
+        progressBar.setVisibility(View.VISIBLE);
+
+    }
+    // To handle "Back" key press event for WebView to go back to previous screen.
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // TODO Auto-generated method stub
+        if((keyCode == KeyEvent.KEYCODE_BACK) && web.canGoBack())
+        {
+            web.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
 }
+
